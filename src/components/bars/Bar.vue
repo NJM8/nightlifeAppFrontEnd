990ee011a336a44a7ced746a1ef8ec314f3b5f24
Nate Mallison <healthynate@gmail.com>
2018-06-27 08:20:10 -0400

    added yelp logo to static, basic bar layout done

--- src/components/bars/Bar.vue
+++ src/components/bars/Bar.vue
@@ -1,28 +1,38 @@
 <template>
-  <div class="col-10 mx-auto mt-4 text-white d-flex">
+  <div class="col-11 mx-auto mt-4 text-white d-flex">
     <div>
       <img
         :src="bardata.image_url"
         :alt="bardata.alias"
         class="image border border-white rounded mt-2">
     </div>
-    <div class="ml-4 mt-4">
+    <div class="ml-4 mt-3 col-4">
       <h3>{{ bardata.name }}</h3>
       <p>Distance Away: {{ distanceInMiles }} miles</p>
       <p>Phone: {{ bardata.display_phone }}</p>
       <p>Address: {{ bardata.location.display_address[0] }}</p>
       <p>{{ bardata.location.display_address[bardata.location.display_address.length - 1] }}</p>
     </div>
-    <div class="ml-4 mt-4">
+    <div class="ml-4 mt-2">
       <div>
         <img
           :src="getRatingPng"
-          alt="rating"><p>Rating: {{ bardata.rating }}</p>
+          alt="rating">
+        <a :href="bardata.url"><img
+          src="/static/YelpLogo_Trademark/Screen(R)/Yelp_trademark_RGB_outline.png"
+          alt="Yelp Logo"
+          class="yelpLogo"
+          target="_blank"
+          rel="noopener"></a>
       </div>
-      <p>yelp logo link</p>
-      <p>See who is here</p>
-      <p>Be Here</p>
-      <p>Share on twitter</p>
+      <p>{{ bardata.rating }} stars based on {{ bardata.review_count }} reviews</p>
+      <div
+        class="btn-group mt-3"
+        role="group">
+        <button class="btn btn-sm btn-outline-info text-white">See who is here</button>
+        <button class="btn btn-sm btn-outline-primary text-white">Be Here</button>
+      </div>
+      <button class="btn btn-sm btn-outline-success text-white mt-2">Share on twitter</button>
     </div>
   </div>
 </template>
@@ -49,6 +59,18 @@ export default {
           return '/static/yelp_stars/web_and_ios/regular/regular_4_half.png'
         case 4:
           return '/static/yelp_stars/web_and_ios/regular/regular_4.png'
+        case 3.5:
+          return '/static/yelp_stars/web_and_ios/regular/regular_3_half.png'
+        case 3:
+          return '/static/yelp_stars/web_and_ios/regular/regular_3.png'
+        case 2.5:
+          return '/static/yelp_stars/web_and_ios/regular/regular_2_half.png'
+        case 2:
+          return '/static/yelp_stars/web_and_ios/regular/regular_2.png'
+        case 1.5:
+          return '/static/yelp_stars/web_and_ios/regular/regular_1_half.png'
+        case 1:
+          return '/static/yelp_stars/web_and_ios/regular/regular_1.png'
         default:
           return '/static/yelp_stars/web_and_ios/regular/regular_0.png'
       }
@@ -61,5 +83,12 @@ export default {
 .image {
   width: 225px;
   height: 225px;
+}
+p {
+  margin-top: 5px;
+  margin-bottom: 0 !important;
+}
+.yelpLogo {
+  width: 100px;
 }
 </style>
